eca8ddbd22525c08f4a63274e95012d6a63b9d5f
Zak Hassan <zak.hassan1010@gmail.com>
2017-05-17 13:18:31 -0400

    Adding different checkpoint dir

--- pyorder-processing.py
+++ pyorder-processing.py
@@ -51,7 +51,7 @@ if __name__ == "__main__":
     query = df\
         .writeStream\
         .option('path','hdfs://192.168.33.40:54310/orders/devnet/warehouse')\
-        .option('checkpointLocation','hdfs://192.168.33.40:54310/orders/devnet/check2')\
+        .option('checkpointLocation','hdfs://192.168.33.40:54310/orders/devnet/check3')\
         .format('parquet')\
         .start()
 
